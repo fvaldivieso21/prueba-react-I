29dcdee8ef0e9d74365c6520d39bf5f876d1df78
fvaldivieso21 <fvaldivieso21@gmail.com>
2023-08-27 19:43:41 -0300

    Update MiCard.jsx

--- src/components/MiCard.jsx
+++ src/components/MiCard.jsx
@@ -5,22 +5,16 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function MiCard({name,img,level}) {
     
     return(
-        <>
-       
+  <>
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={img} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroup.Item>Nivel: {level}</ListGroup.Item>
-        
       </ListGroup>
-      
     </Card>
-  
-
    </>
     )
 }
