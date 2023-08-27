39d1aefa64daad11d9a77f022b7b64081cc1a009
fvaldivieso21 <fvaldivieso21@gmail.com>
2023-08-26 23:14:56 -0300

    agregado Buscador
    Se agrega Buscador y se filtra la lista por nombre

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import "../assets/css/Buscador.css"
 
 
 
-function Buscador(props) {
-
-
-
+function Buscador({ onChange, search }) {
     return (
-        <>
-        </>
-    )
-    
-}
+      <div className="contenedor-buscador">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Buscar un digimon"
+            value={search}
+            onChange={onChange}
+          />
+      </div>
+    );
+  }
 
 export default Buscador;
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import MiCard from "./MiCard";
 import "../assets/css/MiApi.css"
+import Buscador from "./Buscador";
 
 function MiApi() {
+  
   const [series, setSeries] = useState([]);
-  const [buscador, setBuscador] = useState("");
+  const [search, setSearch] = useState("");
 
   const getSeries = async () => {
     const url = 'https://digimon-api.vercel.app/api/digimon';
     const res = await fetch(url);
-    
     const data = await res.json();
-    console.log(data);
-    setSeries(data);
+    const jsonOrdenado = data.sort((a,b) => a.name.localeCompare(b.name));
+
+    setSeries(jsonOrdenado);
+  };
+
+  
+  const handleChange = (e) => {
+    setSearch(e.target.value);
   };
 
   useEffect(() => {
-    
-
     getSeries();
   }, []);
     
     return(
-        <div className="contenedor-cards">
-             {series.map((c,index) => (
-             <MiCard key={index} name={c.name} img={c.img} level={c.level}/>
+        <div>
+          <Buscador onChange={handleChange} search={search} />
+          <div className="contenedor-cards">
+             
+             {series.filter((item) =>
+              item.name.
+              toLowerCase().
+              includes(search.toLowerCase())).
+              map((c,index) => (
+              <MiCard key={index} name={c.name} img={c.img} level={c.level}/>
              ))}
+          </div>
         </div>
     );
 }
